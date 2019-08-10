9a41a2349d5348a64f60cc64b8cdbf602e3f05f9
Adrian Barnes <adrian271@gmail.com>
2019-08-09 22:12:34 -0700

    Create basic webview

--- webviewTest/ViewController.swift
+++ webviewTest/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.google.com")
+        let request = URLRequest(url:url!)
+        
+        webView.load(request)
     }
 
-
+    
+    @IBAction func back(_ sender: Any) {
+        
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    
+    @IBAction func forward(_ sender: Any) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    
+    @IBAction func refresh(_ sender: Any) {
+        webView.reload()
+    }
+    
+    
+    
+    @IBAction func stop(_ sender: Any) {
+        webView.stopLoading()
+    }
+    
+    
 }
 
